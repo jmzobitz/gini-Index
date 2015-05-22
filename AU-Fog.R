6781d5309712c5469def38626e6414059fbe5429
McSafi <safim@T430Y12-R9T600D.ad.augsburg.edu>
2015-05-22 14:07:12 -0500

    I cannot get the plot function to call upon "AU-Fog.R" data set.

--- AU-Fog.R
+++ AU-Fog.R
@@ -6,4 +6,15 @@ for (siteLoop in 1 : length(siteNames)) {   # Loop through the vector of sites
   print(currFile)  # Monitor what file we are analyzing
   siteData = get(load(currFile));   # load file into the workspace
   
-}
+} 
+install.packages('Gini-RData')
+
+
+plot(x = outData\$data, y = outData\$flags)
+plot('AU-Fog.Rda', xlim=range(0,25),)
+dim('AU-Fog.Rda')
+
+fog = data("AU-Fog.Rda")
+plotPoints( temp ~ month, data=fog, xlab="Month (Jan=1, Dec=12)", ylab="Temperature (F)",  
+            main="Ave. Monthly Temp.", xlim=range(0,12), ylim=range(0,80))
+
