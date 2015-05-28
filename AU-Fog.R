596d198f714e43ad7e54f9f883c9e83611597eed
McSafi <safim@T430Y12-R9T600D.ad.augsburg.edu>
2015-05-28 12:02:35 -0500

    Updated AU-Fog

--- AU-Fog.R
+++ AU-Fog.R
@@ -9,7 +9,7 @@ for (siteLoop in 1 : length(siteNames)) {   # Loop through the vector of sites
 } 
 install.packages('Gini-RData')
 
-
+names('Gini-RData')
 plot(x = outData\$data, y = outData\$flags)
 plot('AU-Fog.Rda', xlim=range(0,25),)
 dim('AU-Fog.Rda')
@@ -17,4 +17,13 @@ dim('AU-Fog.Rda')
 fog = data("AU-Fog.Rda")
 plotPoints( temp ~ month, data=fog, xlab="Month (Jan=1, Dec=12)", ylab="Temperature (F)",  
             main="Ave. Monthly Temp.", xlim=range(0,12), ylim=range(0,80))
+scan("AU-Fog.Rda")
+scan('Gini-RData')
 
+plot(NEE)
+plot(GPP)
+plot(TER)
+plot(-GPP+TER)
+
+NEE_SUM=cumsum(NEE)
+plot(NEE_SUM)
